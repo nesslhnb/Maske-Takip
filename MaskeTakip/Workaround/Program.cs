6c1d43a7a011a8ac8d1136c85dfde1abff349029
nesslhnb <neslihanbukte20@gmail.com>
2024-02-21 18:52:41 +0300

    Maske Takip
    2024 Yazılım Geliştirici Yetiştirme Kampı (C#) 1.Gün Ödev 2

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "esra";
+            person1.LastName = "arslan";
+            person1.DateOfBirthYear = 2002;
+            person1.NationalIdentity = 12345678987;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+           
+        }
+    }
+   
+}
